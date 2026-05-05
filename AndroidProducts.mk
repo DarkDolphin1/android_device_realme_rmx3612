c57e79c20e304abbac3c53b0e87c9d214767292c
darkDolphin1 <shravanrajapure1@gmail.com>
2026-05-05 17:17:00 +0530

    Add lunch targets
    Signed-off-by: darkDolphin1 <shravanrajapure1@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,13 @@
 #
-# SPDX-FileCopyrightText: The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_rmx3612.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_rmx3612-user \
+    lineage_rmx3612-userdebug \
+    lineage_rmx3612-eng
